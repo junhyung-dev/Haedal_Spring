88d5b2781b9dbf3b0619dfd52f7b33670b992e96
junhyung-dev <junhyung-dev@gmail.com>
2025-03-26 19:45:51 +0900

    First Commit

---
+++ src/main/java/haedalSpring2025/myFirstProj/MyFirstProjApplication.java
@@ -0,0 +1,13 @@
+package haedalSpring2025.myFirstProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haedalSpring2025/myFirstProj/MyFirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package haedalSpring2025.myFirstProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
